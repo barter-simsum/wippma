471fc2d2eeac95704890a2e6e6d3f2c58db4c501
barter-simsum <barter-simsum@protonmail.com>
2023-09-21 14:14:45 -0400

    rev2 wip

--- btree2.c
+++ btree2.c
@@ -11,7 +11,8 @@
 #include <errno.h>
 #include <string.h>
 
-typedef uint32_t pgno_t;
+typedef uint32_t pgno_t;        /* a page number */
+typedef uint32_t vaof_t;        /* a virtual address offset */
 typedef uint32_t flag_t;
 typedef unsigned char BYTE;
 typedef unsigned long ULONG;
@@ -53,64 +54,111 @@ STATIC_ASSERT(0, "debugger break instruction unimplemented");
 #define SUCC(x) ((x) == BT_SUCC)
 
 
-#define BT_MAPADDR  ((void*)0x10000) /* ;;: change constant */
+/* #define BT_MAPADDR  ((void*)0x10000) /\* ;;: change constant *\/ */
+#define BT_MAPADDR  ((void*)0x200000000000) /* ;;: change constant */
 
 #define BT_PAGEBITS 14ULL
 #define BT_PAGEWORD 32ULL
 #define BT_PAGESIZE (1ULL << BT_PAGEBITS) /* 16K */
 #define BT_ADDRSIZE (BT_PAGESIZE << BT_PAGEWORD)
-#define FO2BY(fo) ((fo) << BT_PAGEBITS)
-
+/*
+  FO2BY: file offset to byte
+  get byte index into pma map from file offset
+*/
+#define FO2BY(fo)                               \
+  ((uint64_t)(fo) << BT_PAGEBITS)
+/*
+  FO2PA: file offset to page
+  get a reference to a BT_page from a file offset
+*/
+#define FO2PA(map, fo)                          \
+  ((BT_page *)&(map)[FO2BY(fo)])
 
 
 //// ===========================================================================
 ////                                  btree types
 
 /*
-  btree page header. all pages share this header. Though for normal pages (not
-  meta and not root) it will be zeroed out
+  btree page header. all pages share this header. Though for metapages, you can
+  expect it to be zeroed out.
 */
 typedef struct BT_pageheader BT_pageheader;
 struct BT_pageheader {
-  uint32_t ver;                 /* version ;;: todo. make a decision. we can unconditionally store this in all pages or simply store it in BT_metadata. Same goes for the rest of the values */
-  uint32_t chk;                 /* checksum */
-  uint32_t blk_base[8];         /* block base array for striped node partition */
-  uint8_t  blk_cnt;             /* currently highest valid block base */
-  uint8_t  depth;               /* tree depth */
-#define BP_DIRTY 0x01           /* for exclusive use by root page */
-#define BP_META  0x02
-/* #define	BP_BRANCH	 0x04 */
-/* #define	BP_LEAF		 0x08 */
-  uint8_t  flags;
-  uint8_t  _pad0;
   uint8_t  dirty[256];          /* dirty bit map */
-  uint32_t  _pad1;
 } __packed;
 
 /*
   btree key/value data format
+
+/*
+  BT_dat is used to provide a view of the data section in a BT_page where data is
+  stored like:
+        va  fo  va  fo
+  bytes 0   4   8   12
+
+  The convenience macros given an index into the data array do the following:
+  BT_dat_lo(i) returns ith   va (low addr)
+  BT_dat_hi(i) returns i+1th va (high addr)
+  BT_dat_fo(i) returns ith file offset
 */
+typedef union BT_dat BT_dat;
+union BT_dat {
+  vaof_t va;                    /* virtual address offset */
+  pgno_t fo;                    /* file offset */
+};
+
+/* like BT_dat but when a struct is more useful than a union */
 typedef struct BT_kv BT_kv;
 struct BT_kv {
-  uint32_t va;                  /* virtual address offset */
-  pgno_t   fo;                  /* file offset */
-} __packed;
+  vaof_t va;
+  pgno_t fo;
+};
+
+/* /\* ;;: tmp tmp tmp *\/ */
+/* typedef struct BT_kvk BT_kvk; */
+/* struct BT_kvk { */
+/*   vaof_t lo; */
+/*   pgno_t fo; */
+/*   vaof_t hi; */
+/* }; */
+
+/* ;;: todo, perhaps rather than an index, return the data directly and typecast?? */
+#define BT_dat_lo(i) ((i) * 2)
+#define BT_dat_fo(i) ((i) * 2 + 1)
+#define BT_dat_hi(i) ((i) * 2 + 2)
+
+#define BT_dat_lo2(I, dat) 
+#define BT_dat_fo2(I, dat)
+#define BT_dat_hi2(I, dat)
+
+/* BT_dat_maxva: pointer to highest va in page data section */
+#define BT_dat_maxva(p)                         \
+  ((void *)&(p)->datd[BT_dat_lo(BT_DAT_MAXKEYS)])
+
+/* BT_dat_maxfo: pointer to highest fo in page data section */
+#define BT_dat_maxfo(p)                         \
+  ((void *)&(p)->datd[BT_dat_fo(BT_DAT_MAXVALS)])
+
+#define BT_DAT_MAXBYTES (BT_PAGESIZE - sizeof(BT_pageheader))
+#define BT_DAT_MAXENTRIES  (BT_DAT_MAXBYTES / sizeof(BT_dat))
+#define BT_DAT_MAXKEYS (BT_DAT_MAXENTRIES / 2)
+#define BT_DAT_MAXVALS BT_DAT_MAXKEYS
+static_assert(BT_DAT_MAXENTRIES % 2 == 0);
 
 /*
    all pages in the memory arena consist of a header and data section
 */
-#define BT_DAT_MAXBYTES (BT_PAGESIZE - sizeof(BT_pageheader))
-#define BT_DAT_MAXKEYS  (BT_DAT_MAXBYTES / sizeof(BT_kv))
 typedef struct BT_page BT_page;
 struct BT_page {
-  BT_pageheader head;
+  BT_pageheader head;           /* ;;: TODO remove header and store all header data in BT_meta */
   union {                       /* data section */
-    BT_kv dat[BT_DAT_MAXKEYS];  /* va:fo pairs */
-    BYTE  datc[0];              /* byte-level view */
+    BT_dat      datd[BT_DAT_MAXENTRIES]; /* union view */
+    BT_kv       datk[0];                 /* struct view */
+    BYTE        datc[0];                 /* byte-level view */
   };
 };
 static_assert(sizeof(BT_page) == BT_PAGESIZE);
-static_assert(BT_DAT_MAXBYTES % sizeof(BT_kv) == 0);
+static_assert(BT_DAT_MAXBYTES % sizeof(BT_dat) == 0);
 
 #define BT_MAGIC   0xBADDBABE
 #define BT_VERSION 1
@@ -126,7 +174,16 @@ struct BT_meta {
   uint32_t  _pad0;
   uint64_t  txnid;
   void     *fix_addr;           /* fixed addr of btree */
-  pgno_t    root;
+
+  uint32_t chk;                 /* checksum */
+  pgno_t   blk_base[8];         /* block base array for striped node partition */
+  uint8_t  blk_cnt;             /* currently highest valid block base */
+  uint8_t  depth;               /* tree depth */
+#define BP_DIRTY 0x01
+#define BP_META  0x02
+  uint8_t  flags;
+  uint8_t  _pad1;
+  pgno_t   root;
 } __packed;
 static_assert(sizeof(BT_meta) <= BT_DAT_MAXBYTES);
 
@@ -135,17 +192,17 @@ static_assert(sizeof(BT_meta) <= BT_DAT_MAXBYTES);
 
 typedef struct BT_state BT_state;
 struct BT_state {
-  uint16_t      flags;
+  uint16_t      flags;          /* ;;: rem */
   int           data_fd;
-  int           meta_fd;
+  int           meta_fd;        /* ;;: confident can be removed because we're not explicitly calling write() */
   char         *path;
-  ULONG         branch_page_cnt;
-  ULONG         leaf_page_cnt;
-  ULONG         depth;
+  ULONG         branch_page_cnt;          /* ;;: rem */
+  ULONG         leaf_page_cnt;          /* ;;: rem */
+  ULONG         depth;          /* ;;: rem */
   void         *fixaddr;
   BYTE         *map;
   BT_meta      *meta_pages[2];  /* double buffered */
-  unsigned int  toggle;         /* which double-buffered db are we using? */
+  unsigned int  which;          /* which double-buffered db are we using? */
 };
 
 
@@ -153,6 +210,231 @@ struct BT_state {
 //// ===========================================================================
 ////                            btree internal routines
 
+/* typedef struct BT_findpath BT_findpath; */
+/* struct BT_findpath { */
+/*   BT_dat *path[4];              /\* ;;: todo #DEFINE maxdepth *\/ */
+/*   size_t idx[4];                /\* ;;: why do we need idx again? We're storing */
+/*                                      POINTERS to the dat *\/ */
+/*   uint8_t depth; */
+/* }; */
+
+typedef struct BT_findpath BT_findpath;
+struct BT_findpath {
+  BT_page *path[4];             /* ;;: todo #DEFINE maxdepth */
+  size_t idx[4];                /* ;;: why do we need idx again? We're storing
+                                     POINTERS to the dat */
+  uint8_t depth;
+};
+
+/* ;;: duh */
+
+/* static int */
+/* _bt_findpath_new(BT_findpath **path) */
+/* { */
+/*   BT_findpath *p = calloc(1, sizeof *p); */
+/*   *path = p; */
+/*   return BT_SUCC; */
+/* } */
+
+/* get_node: get a pointer to a node stored at file offset pgno */
+static BT_page *
+_get_node(BT_state *state, pgno_t pgno)
+{
+  /* TODO: eventually, once we can store more than 2M of nodes, this will need
+     to reference the meta page's blk_base array to determine where a node is
+     mapped. i.e:
+
+  - receive pgno
+  - find first pgno in blk_base that exceeds pgno : i
+  - sector that contains node is i-1
+  - appropriately offset into i-1th fixed size partition: 2M, 8M, 16M, ...
+
+  */
+
+  /* for now, this works because the 2M sector is at the beginning of both the
+     memory arena and pma file
+  */
+  /* ;;: fix assert - 2M */
+  /* assert(); */
+  return FO2PA(state->map, pgno);
+}
+
+/* binary search a page's data section for a va. Returns a pointer to the found BT_dat */
+static void *
+_bt_bsearch(BT_page *page, vaof_t va)
+{
+  /* ;;: todo: actually bsearch rather than linear */
+  for (BT_kv *kv = &page->datk[0]; kv <= BT_dat_maxva(page); kv++) {
+    if (kv->va == va)
+      return kv;
+  }
+
+  return 0;
+}
+
+#define is_leaf(d1, d2) ((d1) == (d2))
+#define is_branch(d1, d2) (!is_leaf(d1, d2))
+
+/* ;;: find returns a path to nodes that things should be in if they are there. */
+/* a leaf has a meta page depth eq to findpath depth */
+
+/* ;;: I don't believe we actually need this findpath struct. See introduction
+     to algorithms chp 18: B-Trees */
+static int
+_bt_find2(BT_page *page,
+          BT_findpath *path,
+          uint8_t maxdepth,
+          vaof_t lo,
+          vaof_t hi)
+{
+  /* ;;: meta page stores depth (node or leaf?)
+     look at root node and binsearch BT_dats where low is <= lo and high is >= hi
+     If at depth of metapage (a leaf), then done
+     otherwise grab node, increment depth, save node in path
+  */
+  uint8_t depth = path->depth;
+  
+  if (path->depth > maxdepth)
+    return ENOENT;
+
+  
+  /* ;;: rev1 */
+  BT_dat *dat, *d;
+  dat = d = &page->datd[0];
+  if (is_leaf(depth, maxdepth)) {
+    for (; d < BT_dat_maxva(page); d += 2) {
+      vaof_t llo = d->va;
+      vaof_t hhi = (d + 2)->va;
+      if (llo <= lo && hhi >= hi) {
+        path->idx[path->depth] = d / 2;
+        path->path[path->depth] = page;
+        return BT_SUCC;
+      }
+    }
+    return ENOENT;
+  }
+  /* then branch */
+  else {
+    for (; d < BT_dat_maxva(page); d += 2) {
+      vaof_t llo = d->va;
+      vaof_t hhi = (d + 2)->va;
+      if (llo <= lo && hhi >= hi) {
+        pgno_t fo = (d + 1)->fo;
+        BT_page *child = _get_node(state, fo);
+        path->idx[depth] = d;
+        path->path[depth] = page;
+        path->depth++;
+        return _bt_find2(child, path, maxdepth, lo, hi);
+      }
+    }
+    return ENOENT;
+  }
+
+  
+  /* ;;: rev2 */
+  size_t i = 0;
+  if (is_leaf(depth, maxdepth)) {
+    for (; i < BT_DAT_MAXKEYS - 1; i++) {
+      vaof_t llo = page->datk[i].va;
+      vaof_t hhi = page->datk[i + 1].va;
+      if (llo <= lo && hhi >= hi) {
+        path->idx[path->depth] = i;
+        path->path[path->depth] = page;
+        return BT_SUCC;
+      }
+    }
+    return ENOENT;
+  }
+  /* then branch */
+  else {
+    for (; i < BT_DAT_MAXKEYS - 1; i++) {
+      vaof_t llo = page->datk[i].va;
+      vaof_t hhi = page->datk[i + 1].va;
+      if (llo <= lo && hhi >= hi) {
+        pgno_t fo = page->datk[i].fo;
+        BT_page *child = _get_node(state, fo);
+        path->idx[depth] = i;
+        path->path[depth] = page;
+        path->depth++;
+        return _bt_find2(child, path, maxdepth, lo, hi);
+      }
+    }
+    return ENOENT;
+  }
+  
+  /* /\* ;;: *\/ */
+  /* BT_kv *kv = &page->datk[0]; */
+  /* if (is_leaf(depth, maxdepth)) { */
+  /*   for (; kv <= BT_dat_maxva(page); kv++) { */
+  /*     if (kv->va == lo) { */
+  /*       path->path[depth] = (BT_dat *)&kv->fo; */
+  /*       return BT_SUCC; */
+  /*     } */
+  /*   } */
+  /*   return ENOENT; */
+  /* }      if (llo <= lo && hhi >= hi) { */
+  /* /\* then branch *\/ */
+  /* else { */
+  /*   for (; kv <= BT_dat_maxva(page); kv++) { */
+  /*     /\* found entry (but overstepped by one) *\/ */
+  /*     if (kv->va >= hi) { */
+  /*       assert(kv != &page->datk[0]); */
+  /*       BT_kv *ent = kv - 1; */
+  /*       path->path[depth] = (BT_dat *)ent; */
+  /*       path->depth = depth++; */
+  /*       return _bt_find2(page, path, maxdepth, lo, hi); */
+  /*     } */
+  /*   } */
+  /*   return ENOENT; */
+  /* } */
+}
+
+static int
+_bt_find(BT_state *state, BT_findpath *path, vaof_t lo, vaof_t hi)
+{
+  BT_meta *meta = state->meta_pages[state->which];
+  BT_page *root = _get_node(state, meta->root);
+  uint8_t maxdepth = meta->depth;
+  return _bt_find2(root, path, maxdepth, lo, hi);
+}
+
+static int
+_bt_split(BT_findpath *path, BT_page *node)
+{
+  /* split actually does the CoW for _bt_insert. bt_insert doesn't for non-full
+     nodes */
+  return 255;
+}
+
+static int
+_bt_insert(BT_state *state, vaof_t lo, vaof_t hi, pgno_t fo)
+{
+  BT_findpath path = {0};
+  int rc;
+  if (!SUCC(rc = _bt_find(state, &path, lo, hi))) {
+    return rc;
+  }
+  /* path now contains PATH to leaf node at path->depth handle splitting if
+    necessary. Check all nodes in path if need to be split
+  */
+  /* "implement" basic function that bumps node allocation pointer. store
+     BT_page[2M / sizeof(BT_page)] array in state. error out when size is
+     exceeded.
+  */
+  return;
+}
+
+static int
+_bt_delete()
+{
+  return 255;
+}
+
+/* bt_insert. Find interval (via _bt_find) and see if there's space in leaf to
+   insert. If not, split leaf, go up path by 1, etc.
+
+   return succ/fail
+*/
 
 #define CLOSE_FD(fd)                            \
   do {                                          \
@@ -197,14 +479,14 @@ _bt_state_meta_new(BT_state *state, BT_meta *meta)
   meta->last_pg = 1;
   meta->txnid = 0;
   meta->fix_addr = BT_MAPADDR;
+  meta->blk_cnt = 1;
+  meta->depth = 1;
+  meta->flags = BP_META;
   meta->root = UINT32_MAX;      /* ;;: actually should probably be 0 */
 
   /* initialize the metapages */
   p1 = calloc(2, BT_PAGESIZE);
   p2 = p1 + 1;
-  p2->head.flags = p1->head.flags = BP_META;
-  p2->head.blk_cnt = p1->head.blk_cnt = 1;
-  p2->head.depth = p1->head.depth = 1;
 
   /* copy the metadata into the metapages */
   memcpy(METADATA(p1), meta, sizeof *meta);
@@ -235,7 +517,7 @@ _bt_state_load(BT_state *state)
   int new = 0;
   BT_meta meta = {0};
   BT_page *p = 0;
-  
+
   TRACE();
 
   if (!SUCC(rc = _bt_state_read_header(state, &meta))) {
@@ -300,7 +582,6 @@ bt_state_open(BT_state *state, const char *path, ULONG flags, mode_t mode)
 
   TRACE();
   UNUSED(flags);
-  UNUSED(path);                 /* TODO use path */
 
   oflags = O_RDWR | O_CREAT;
   dpath = malloc(strlen(path) + sizeof(DATANAME));
@@ -313,6 +594,7 @@ bt_state_open(BT_state *state, const char *path, ULONG flags, mode_t mode)
   if (!SUCC(rc = _bt_state_load(state)))
     goto e;
 
+  /* ;;: this may be entirely unnecessary */
   oflags |= O_DSYNC;            /* see man 2 open */
   if ((state->meta_fd = open(dpath, oflags, mode)) == -1) {
     rc = errno;
